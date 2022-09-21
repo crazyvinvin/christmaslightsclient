72bdc539aec91df0c5cc66799ea6f4f4e0d592d5
Vermeire,Vincent V.L <v.vermeire@student.fontys.nl>
2022-09-21 20:58:44 +0200

    First Release

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import React from 'react';
 
 function App() {
+
+  const baseUrl = "https://192.168.42.44:7234/api/Lights";
+  const [christmafyingStatus, setChristmafyingStatus] = React.useState("");
+
+  async function Christmafy(){
+    const groupsSelect = document.getElementById("groups");
+    const selectedGroup = groupsSelect.value;
+    setChristmafyingStatus(`Christmafying ${selectedGroup} ...`)
+    console.log(christmafyingStatus)
+    await axios.post(baseUrl + "/Christmafy",  null, {
+      params: {
+        groupName: selectedGroup
+      }
+    })
+    setChristmafyingStatus('Christmafied')
+  }
+
+  function resetChristmafyingStatus(){
+    setChristmafyingStatus("")
+  }
+  const [groups, setGroups] = React.useState(null);
+  React.useEffect(() => {
+    axios.get(baseUrl + "/groups").then((response) => {
+      setGroups(response.data);
+    });
+  }, []);
+
+  if (!groups) return "GROUPS WAS NULL"
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="groupSelectText">Choose a group:</h1>
+      <select className="groupSelect" name="groups" id="groups" onClick={resetChristmafyingStatus}>
+        {groups.map((group, index) => (
+          <option key={index} value={group}>
+            {group}
+          </option>
+        ))}
+      </select>
+      <button className="christmafyButton" onClick={Christmafy}>
+        Christmafy!
+      </button>
+      <h2 className="christmafyingStatusText linear-wipe">{christmafyingStatus}</h2>
     </div>
   );
 }
